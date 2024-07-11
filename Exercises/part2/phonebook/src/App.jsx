b181f3ddc23e365dbf079f5d45bdde564307bb52
kvardaan <vardaankhattar8907@gmail.com>
2024-07-11 19:57:33 +0530

    Part 2.c

--- Exercises/part2/phonebook/src/App.jsx
+++ Exercises/part2/phonebook/src/App.jsx
@@ -4,13 +4,8 @@ import { Persons } from "./components/Persons"
 import { PersonForm } from "./components/PersonForm"
 import { Filter } from "./components/Filter"
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+const App = (props) => {
+  const [persons, setPersons] = useState(props.persons)
 
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
